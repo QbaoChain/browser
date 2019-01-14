534ae2e013a38642f3b14ea9cbfcc5d31965a3d2
hepf <hepf33@hotmail.com>
2019-01-14 13:52:38 +0800

    add constant contract methods

--- src/constant/common.js
+++ src/constant/common.js
@@ -1,20 +1,24 @@
 export const asmDataType = {
     origin: {
-        name: "原始",
-        value: "origin"
+        name: '原始',
+        value: 'origin'
     },
     plain: {
-        name: "文本",
-        value: "plain"
+        name: '文本',
+        value: 'plain'
     },
     method: {
-        name: "方法",
-        value: "method"
+        name: '方法',
+        value: 'method'
     },
     picture: {
-        name: "图片",
-        value: "picture"
+        name: '图片',
+        value: 'picture'
     }
 };
 
-export const imgBase64Prefix = "data:image;base64,";
+export const imgBase64Prefix = 'data:image;base64,';
+
+export const contractMethods = {
+    'a9059cbb': 'transfer(address,uint256)'
+}
